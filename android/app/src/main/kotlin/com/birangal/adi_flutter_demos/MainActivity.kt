49084c3369a5fed448d2e960a5062a3961067836
Aditya Birangal <aditya.birangal@gmail.com>
2020-12-06 14:48:29 +0530

    Google SignIn (Auto)

---
+++ android/app/src/main/kotlin/com/birangal/adi_flutter_demos/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.birangal.adi_flutter_demos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+// Copyright (c) 2020 Aditya Birangal
+
+import 'package:flutter/material.dart';
+import 'package:adi_flutter_demos/profile.dart';
+import 'package:adi_flutter_demos/sign_in.dart';
+
+import 'login_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    getUserInfo();
+  }
+
+  Future getUserInfo() async {
+    await getUser();
+    setState(() {});
+    print(uid);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Login',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: (uid != null && authSignedIn != false)
+          ? ProfileScreen()
+          : LoginPage(),
+    );
+  }
+}
